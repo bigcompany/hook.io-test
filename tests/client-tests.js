db01f81729fb5a04e03cb307ee20e7638f9e5515
Marak <marak.squires@gmail.com>
2015-09-26 19:40:00 -0700

    [tests] Add `request` based client tests  * Covers most basic echo hook params cases

---
+++ tests/client-tests.js
@@ -0,0 +1,111 @@
+var tap = require("tape");
+
+var r = require('../lib/helpers/_request');
+
+var baseURL = "http://localhost:9999"
+
+/*
+
+    HOOK PARAMETER TESTS
+
+*/
+
+tap.test('get the echo hook', function (t) {
+  r({ uri: baseURL + "/marak/echo" }, function (err, echo) {
+    t.error(err);
+    t.equal(echo.owner, "marak");
+    t.equal(echo.hook, "echo");
+    t.end();
+  });
+});
+
+tap.test('get the echo hook with query string parameters', function (t) {
+  r({ uri: baseURL + "/marak/echo?baz=boz" }, function (err, echo) {
+    t.error(err);
+    t.equal(echo.owner, "marak");
+    t.equal(echo.hook, "echo");
+    t.equal(echo.baz, "boz");
+    t.end();
+  });
+});
+
+tap.test('post to the echo hook', function (t) {
+  r({ uri: baseURL + "/marak/echo", method: "post" }, function (err, echo) {
+    t.error(err);
+    t.equal(echo.owner, "marak");
+    t.equal(echo.hook, "echo");
+    t.end();
+  });
+});
+
+/*
+
+    JSON TESTS
+
+*/
+
+tap.test('post to the echo hook with JSON data', function (t) {
+  r({ 
+    uri: baseURL + "/marak/echo", 
+    method: "post",
+    json: {
+      "baz": "boz"
+    }
+  }, function (err, echo) {
+    t.error(err);
+    t.equal(echo.owner, "marak");
+    t.equal(echo.hook, "echo");
+    t.equal(echo.baz, "boz");
+    t.end();
+  });
+});
+
+/*
+
+    FORM SUBMIT TESTS
+
+*/
+
+tap.test('submit a URL-encoded form to the echo hook with form data', function (t) {
+  r({ 
+    uri: baseURL + "/marak/echo", 
+    method: "post",
+    form: {
+      "baz": "boz"
+    }
+  }, function (err, echo) {
+    t.error(err);
+    t.equal(echo.owner, "marak");
+    t.equal(echo.hook, "echo");
+    t.equal(echo.baz, "boz");
+    t.end();
+  });
+});
+
+/*
+
+    SCHEMA TESTS
+
+*/
+
+tap.test('get the echo hook and check default schema data', function (t) {
+  r({ uri: baseURL + "/marak/echo" }, function (err, echo) {
+    t.error(err);
+    t.equal(echo.owner, "marak");
+    t.equal(echo.hook, "echo");
+    t.equal(echo.param1, "foo");
+    t.equal(echo.param2, "bar");
+    t.end();
+  });
+});
+
+tap.test('post the echo hook and check default schema data', function (t) {
+  r({ uri: baseURL + "/marak/echo", method: "post" }, function (err, echo) {
+    t.error(err);
+    t.equal(echo.owner, "marak");
+    t.equal(echo.hook, "echo");
+    t.equal(echo.param1, "foo");
+    t.equal(echo.param2, "bar");
+    t.end();
+  });
+});
