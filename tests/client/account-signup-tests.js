3ea3adcfda8942fa8140edbbd3ada341716bfa6b
Marak <marak.squires@gmail.com>
2016-06-04 04:12:44 -0400

    [tests] Add setup / teardown for account tests

--- tests/client/account-signup-tests.js
+++ tests/client/account-signup-tests.js
@@ -1,38 +1,25 @@
-// disabled until we have proper teardown for deleting users
-return;  
-// attempt to signup with with un-available account name
-
-// attempt to signup with available email
-  // no password
-  // not matching passwords
-  // valid matching passwords
-  
-// attempt to signup with un-available email
-
-// TODO: we need the ability to delete user accounts in order to perform the tear-down for this test
-
 var tap = require("tape");
 var r = require('../../lib/helpers/_request');
 var baseURL = "http://localhost:9999"
 
+var config = require('../../config');
 
-tap.test('attempt to signup with no account name or email', function (t) {
-  r({ uri: baseURL + "/signup", method: "POST" }, function (err, res) {
-    t.error(err);
-    t.equal(res, 'invalid');
-    t.end();
-  });
-});
-
-var testUser = {
-  name: "bobby-test-hookio"
-};
+var testUser = config.testUsers.bobby;
 
 /*
 
   attempt to signup by account name
 
 */
+
+tap.test('attempt to signup with no account name or email', function (t) {
+  r({ uri: baseURL + "/signup", method: "POST" }, function (err, res) {
+    t.error(err, 'request did not error');
+    t.equal(typeof res, 'object', "response contains object");
+    t.equal(res.result, 'invalid', "is not valid signup");
+    t.end();
+  });
+});
 
 tap.test('attempt to signup by account name with no password', function (t) {
   r({ 
@@ -42,11 +29,16 @@ tap.test('attempt to signup by account name with no password', function (t) {
         "email": testUser.name
       },
     }, function (err, res) {
-      t.error(err);
-      t.equal(res, 'available');
+      t.error(err, 'request did not error');
+      t.equal(typeof res, 'object', "response contains object");
+      t.equal(res.result, 'available', "name is available");
       t.end();
   });
 });
+
+/*
+
+Note: Removed from API
 
 tap.test('attempt to signup by account name mismtached passwords', function (t) {
   r({ 
@@ -58,11 +50,13 @@ tap.test('attempt to signup by account name mismtached passwords', function (t) 
         "confirmPassword": "fooBar"
       },
     }, function (err, res) {
-      t.error(err);
-      t.equal(res, 'available');
+      t.error(err, 'request did not error');
+      t.equal(typeof res, 'object', "response contains object");
+      t.equal(res.result, 'invalid', "passwords do not match");
       t.end();
   });
 });
+*/
 
 tap.test('attempt to signup by account name with valid password', function (t) {
   r({ 
@@ -75,14 +69,23 @@ tap.test('attempt to signup by account name with valid password', function (t) {
       },
     }, function (err, res) {
       t.error(err);
-      t.equal(res.res, 'valid');
+      t.error(err, 'request did not error');
+      t.equal(typeof res, 'object', "response contains object");
+      t.equal(res.result, 'valid', "name is available");
       t.end();
   });
 });
 
-return;
-// not matching passwords
-// valid matching passwords
-
-
-
+tap.test('attempt to clear test user - as superadmin', function (t) {
+  r({ uri: baseURL + "/_admin", method: "POST", json: {
+    method: "user.destroy",
+    super_private_key: config.superadmin.super_private_key,
+    name: testUser.name
+  }}, function (err, res, body) {
+    t.error(err);
+    t.error(err, 'request did not error');
+    t.equal(typeof res, 'object', "response contains object");
+    t.equal(res.result, 'deleted', "deleted user");
+    t.end();
+  });
+});
